ece24577400e80043614770c0ded3810c7c0f60a
Your Name <you@example.com>
2019-06-18 14:17:05 -0300

    adcionando novas funcionalidades no show

--- app/models/noticia.js
+++ app/models/noticia.js
@@ -12,28 +12,32 @@ NoticiaDao.prototype.findAll = function(connection, callback) {
 NoticiaDao.prototype.findById = function(connection, noticia_id, callback) {
 	console.log('\tModel noticia. Operação findById');		
 	/* TODO
-	Implementar a funão findById
+	Implementar a função findById
 	*/
+	connection.query(
+		`SELECT noticia_id, titulo, DATE_FORMAT(data,'%d/%m/%Y') as data, conteudo FROM noticia WHERE noticia_id = ${noticia_id}`, callback
+
+		)
 }
 
 NoticiaDao.prototype.add = function(connection, noticia, callback) {
 	console.log('\tModel noticia. Operação add');			
 	/* TODO
-	Implementar a funão add
+	Implementar a função add
 	*/
 }
 
 NoticiaDao.prototype.edit = function(connection, noticia, callback) {
 	console.log('\tModel noticia. Operação edit');			
 	/* TODO
-	Implementar a funão edit
+	Implementar a função edit
 	*/
 }
 
 NoticiaDao.prototype.delete = function(connection, noticia, callback) {
 	console.log('\tModel noticia. Operação delete');			
 	/* TODO
-	Implementar a funão delete
+	Implementar a função delete
 	*/
 }
 
